d82fa616c4b927ed33fef007c807d8ad01f80322
Jeongbeen Hwang <jeongbeen.hwang@navercorp.com>
2022-09-04 00:53:25 +0900

    master 1

--- example/js/script.js
+++ example/js/script.js
@@ -16,7 +16,7 @@ function addRecord() {
 
         // read records again
         readRecords();
-
+        // hhihihi
         // clear fields from the popup
         $("#first_name").val("");
         $("#last_name").val("");
